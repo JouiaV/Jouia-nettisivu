321537712de4ede6e7097bda3060053500f1c256
JouiaV <127497066+JouiaV@users.noreply.github.com>
2023-03-27 09:43:19 +0300

    Update Pelit.js

--- javascript/Pelit.js
+++ javascript/Pelit.js
@@ -4,3 +4,9 @@ function open_turri_clicker() {
     window.open("Pelit/TurriClicker/turriClicker.html","_self")
 }
 
+
+function open_turri_runner() {
+    window.open("Pelit/TurriRunner/turriRunner.html","_self")
+}
+
+
