ab01d8713114a02e3279267a9496b4e496c81d94
JouiaV <127497066+JouiaV@users.noreply.github.com>
2023-03-31 07:52:07 +0300

    Add files via upload

---
+++ Pelit/TurriRunner/turriRunner_local_storage.js
@@ -0,0 +1,21 @@
+
+
+
+
+let high_score = localStorage.getItem("high-score");
+
+if (high_score === null) {
+    console.log("ei oo")
+    high_score = 0
+    localStorage.setItem("high-score", 0);
+} 
+const highScoreElem = document.querySelector("[data-high-score]")
+highScoreElem.innerHTML = high_score
+
+export function manageNewScore(score) {
+    if (score > high_score) {
+        localStorage.setItem("high-score", score);
+        highScoreElem.innerHTML = score
+    }
+}
+
---
+++ javascript/Pelit.js
@@ -0,0 +1,11 @@
+
+
+function open_turri_clicker() {
+    window.open("Pelit/TurriClicker/turriClicker.html","_self")
+}
+
+function open_turri_runner() {
+    window.open("Pelit/TurriRunner/turriRunner.html","_self")
+}
+
+
