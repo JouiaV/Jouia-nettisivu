98a4c65ad67311f96302c662765328e8e68ffd78
JouiaV <127497066+JouiaV@users.noreply.github.com>
2023-04-08 20:54:52 +0300

    Delete javascript directory

--- javascript/vaipo_mainos.js
+++
@@ -1,7 +0,0 @@
-// MAINOS --------------------------------------------------------------------
-
-sessionStorage.setItem("mainos_naytetty", "true")
-
-function close_mainos() {
-    window.open("index.html","_self")
-}
