0b6678514c824783b40c463846c4f6100498b3b8
JouiaV <127497066+JouiaV@users.noreply.github.com>
2023-04-14 21:31:04 +0300

    2048_karvakamua_baseline

--- javascript/Pelit.js
+++ javascript/Pelit.js
@@ -12,4 +12,6 @@ function open_PörröPet() {
     window.open("Pelit/PörröPet/PörröPet.html", "_self")
 }
 
-
+function open_2048_karvakamua() {
+    window.open("Pelit/2048_karvakamua/2048_karvakamua.html", "_self")
+}
